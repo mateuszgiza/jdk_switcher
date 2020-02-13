efd133e590fcd375c6d08807140254a5e3536d0f
Paweł Grzęda <pawel.grzeda@prosoftlab.pl>
2020-02-13 11:21:03 +0100

    Upload jdk_switcher for FreeBSD

--- jdk_switcher.sh
+++ jdk_switcher.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+echo "before script"
+java -version
 if uname -a | grep x86_64 >/dev/null; then
     ARCH_SUFFIX=amd64
 else
@@ -9,68 +11,20 @@ fi
 : "${JDK_SWITCHER_DEFAULT:=openjdk8}"
 : "${UJA:=update-java-alternatives}"
 
-OPENJDK6_UJA_ALIAS="java-1.6.0-openjdk"
-OPENJDK6_JAVA_HOME="/usr/lib/jvm/java-6-openjdk"
-if [[ -d "/usr/lib/jvm/java-6-openjdk-${ARCH_SUFFIX}" ]]; then
-    OPENJDK6_UJA_ALIAS="java-1.6.0-openjdk-${ARCH_SUFFIX}"
-    OPENJDK6_JAVA_HOME="/usr/lib/jvm/java-6-openjdk-${ARCH_SUFFIX}"
-fi
+MAKE_FILE_PATH="/etc/make.conf"
+OPENJDK_VENDOR="openjdk"
+OPENJDK8_VERSION="8"
+OPENJDK8_JAVA_HOME="/usr/local/openjdk8"
 
-OPENJDK7_UJA_ALIAS="java-1.7.0-openjdk"
-OPENJDK7_JAVA_HOME="/usr/lib/jvm/java-7-openjdk"
-if [[ -d "/usr/lib/jvm/java-7-openjdk-${ARCH_SUFFIX}" ]]; then
-    OPENJDK7_UJA_ALIAS="java-1.7.0-openjdk-${ARCH_SUFFIX}"
-    OPENJDK7_JAVA_HOME="/usr/lib/jvm/java-7-openjdk-${ARCH_SUFFIX}"
-fi
+OPENJDK11_VERSION="11"
+OPENJDK11_JAVA_HOME="/usr/local/openjdk11"
 
-OPENJDK8_UJA_ALIAS="java-1.8.0-openjdk"
-OPENJDK8_JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
-if [[ -d "/usr/lib/jvm/java-8-openjdk-${ARCH_SUFFIX}" ]]; then
-    OPENJDK8_UJA_ALIAS="java-1.8.0-openjdk-${ARCH_SUFFIX}"
-    OPENJDK8_JAVA_HOME="/usr/lib/jvm/java-8-openjdk-${ARCH_SUFFIX}"
-fi
+OPENJDK12_VERSION="12"
+OPENJDK12_JAVA_HOME="/usr/local/openjdk12"
 
-ORACLEJDK7_UJA_ALIAS="java-7-oracle"
-ORACLEJDK7_JAVA_HOME="/usr/lib/jvm/java-7-oracle"
-if [[ -d "/usr/lib/jvm/java-7-oracle-${ARCH_SUFFIX}" ]]; then
-    ORACLEJDK7_UJA_ALIAS="java-7-oracle-${ARCH_SUFFIX}"
-    ORACLEJDK7_JAVA_HOME="/usr/lib/jvm/java-7-oracle-${ARCH_SUFFIX}"
-fi
+OPENJDK13_VERSION="13"
+OPENJDK13_JAVA_HOME="/usr/local/openjdk13"
 
-ORACLEJDK8_UJA_ALIAS="java-8-oracle"
-ORACLEJDK8_JAVA_HOME="/usr/lib/jvm/java-8-oracle"
-if [[ -d "/usr/lib/jvm/java-8-oracle-${ARCH_SUFFIX}" ]]; then
-    ORACLEJDK8_UJA_ALIAS="java-8-oracle-${ARCH_SUFFIX}"
-    ORACLEJDK8_JAVA_HOME="/usr/lib/jvm/java-8-oracle-${ARCH_SUFFIX}"
-fi
-
-ORACLEJDK9_UJA_ALIAS="java-9-oracle"
-ORACLEJDK9_JAVA_HOME="/usr/lib/jvm/java-9-oracle"
-if [[ -d "/usr/lib/jvm/java-9-oracle-${ARCH_SUFFIX}" ]]; then
-    ORACLEJDK9_UJA_ALIAS="java-9-oracle-${ARCH_SUFFIX}"
-    ORACLEJDK9_JAVA_HOME="/usr/lib/jvm/java-9-oracle-${ARCH_SUFFIX}"
-fi
-
-ORACLEJDK10_UJA_ALIAS="java-10-oracle"
-ORACLEJDK10_JAVA_HOME="/usr/lib/jvm/java-10-oracle"
-if [[ -d "/usr/lib/jvm/java-10-oracle-${ARCH_SUFFIX}" ]]; then
-    ORACLEJDK10_UJA_ALIAS="java-10-oracle-${ARCH_SUFFIX}"
-    ORACLEJDK10_JAVA_HOME="/usr/lib/jvm/java-10-oracle-${ARCH_SUFFIX}"
-fi
-
-IBMJAVA8_UJA_ALIAS="java-8-ibm"
-IBMJAVA8_JAVA_HOME="/usr/lib/jvm/java-8-ibm"
-if [[ -d "/usr/lib/jvm/java-8-ibm-${ARCH_SUFFIX}" ]]; then
-    IBMJAVA8_UJA_ALIAS="java-8-ibm-${ARCH_SUFFIX}"
-    IBMJAVA8_JAVA_HOME="/usr/lib/jvm/java-8-ibm-${ARCH_SUFFIX}"
-fi
-
-OPENJDK8_OPENJ9_UJA_ALIAS="openjdk8-openj9"
-OPENJDK8_OPENJ9_JAVA_HOME="/usr/lib/jvm/openjdk8-openj9"
-if [[ -d "/usr/lib/jvm/openjdk8-openj9-${ARCH_SUFFIX}" ]]; then
-    OPENJDK8_OPENJ9_UJA_ALIAS="openjdk8-openj9-${ARCH_SUFFIX}"
-    OPENJDK8_OPENJ9_JAVA_HOME="/usr/lib/jvm/openjdk8-openj9-${ARCH_SUFFIX}"
-fi
 
 for config_file in /etc/default/jdk-switcher "${HOME}/.jdk_switcherrc" "${JDK_SWITCHER_CONFIG}"; do
     if [[ -f "${config_file}" ]]; then
@@ -79,151 +33,38 @@ for config_file in /etc/default/jdk-switcher "${HOME}/.jdk_switcherrc" "${JDK_SW
     fi
 done
 
-switch_to_openjdk6() {
-    echo "Switching to OpenJDK6 ($OPENJDK6_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK6_JAVA_HOME"
-    sudo "${UJA}" --set "$OPENJDK6_UJA_ALIAS"
-    export JAVA_HOME="$OPENJDK6_JAVA_HOME"
+switch_to_specified_openjdk() {
+	JAVA_VERSION="$1"
+	JAVA_HOME="$2"
+	echo "Switching to Openjdk version ($JAVA_VERSION) with java_home ($JAVA_HOME)"
+	create_make_file
+	replace_vendor_and_version "$JAVA_VERSION" "$OPENJDK_VENDOR"
+	echo "Set JAVA_HOME to $JAVA_HOME"
+	set JAVA_HOME="$JAVA_HOME"
+	export JAVA_HOME
 }
 
-switch_to_openjdk7() {
-    echo "Switching to OpenJDK7 ($OPENJDK7_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK7_JAVA_HOME"
-    sudo "${UJA}" --set "$OPENJDK7_UJA_ALIAS"
-    export JAVA_HOME="$OPENJDK7_JAVA_HOME"
+create_make_file() {
+	if [[ -e "$MAKE_FILE_PATH" ]]; then
+	  echo "File make already exists!"
+	else
+	  echo >> "$MAKE_FILE_PATH"
+	fi
 }
 
-switch_to_openjdk8() {
-    echo "Switching to OpenJDK8 ($OPENJDK8_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK8_JAVA_HOME"
-    sudo "${UJA}" --set "$OPENJDK8_UJA_ALIAS"
-    export JAVA_HOME="$OPENJDK8_JAVA_HOME"
+replace_vendor_and_version() {
+	JAVA_VERSION="$1"
+	JAVA_VENDOR="$2"
+	sed -i '' '/^JAVA_VENDOR=/ d' "$MAKE_FILE_PATH"
+	sed -i '' '/^JAVA_VERSION=/ d' "$MAKE_FILE_PATH"
+	echo "JAVA_VENDOR=$JAVA_VENDOR">>"$MAKE_FILE_PATH"
+	echo "JAVA_VERSION=$JAVA_VERSION">>"$MAKE_FILE_PATH"
 }
 
-switch_to_oraclejdk7() {
-    echo "Switching to Oracle JDK7 ($ORACLEJDK7_UJA_ALIAS), JAVA_HOME will be set to $ORACLEJDK7_JAVA_HOME"
-    sudo "${UJA}" --set "$ORACLEJDK7_UJA_ALIAS"
-    export JAVA_HOME="$ORACLEJDK7_JAVA_HOME"
-}
 
-switch_to_oraclejdk8() {
-    echo "Switching to Oracle JDK8 ($ORACLEJDK8_UJA_ALIAS), JAVA_HOME will be set to $ORACLEJDK8_JAVA_HOME"
-    sudo "${UJA}" --set "$ORACLEJDK8_UJA_ALIAS"
-    export JAVA_HOME="$ORACLEJDK8_JAVA_HOME"
-}
-
-switch_to_oraclejdk9() {
-    echo "Switching to Oracle JDK9 ($ORACLEJDK9_UJA_ALIAS), JAVA_HOME will be set to $ORACLEJDK9_JAVA_HOME"
-    sudo "${UJA}" --set "$ORACLEJDK9_UJA_ALIAS"
-    export JAVA_HOME="$ORACLEJDK9_JAVA_HOME"
-}
-
-switch_to_oraclejdk10() {
-    echo "Switching to Oracle JDK10 ($ORACLEJDK10_UJA_ALIAS), JAVA_HOME will be set to $ORACLEJDK10_JAVA_HOME"
-    sudo "${UJA}" --set "$ORACLEJDK10_UJA_ALIAS"
-    export JAVA_HOME="$ORACLEJDK10_JAVA_HOME"
-}
-
-switch_to_ibmjava8() {
-    echo "Switching to IBM JAVA8 ($IBMJAVA8_UJA_ALIAS), JAVA_HOME will be set to $IBMJAVA8_JAVA_HOME"
-    update_jdk_entries $IBMJAVA8_JAVA_HOME
-}
-
-switch_to_openjdk8_openj9() {
-    echo "Switching to OPENJDK8 OPENJ9 ($OPENJDK8_OPENJ9_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK8_OPENJ9_JAVA_HOME"
-    update_jdk_entries $OPENJDK8_OPENJ9_JAVA_HOME
-}
-
-update_jdk_entries() {
-
-    java_home="$1"
-    jre=0
-    dir="$java_home/jre/bin"
-    if [ -d "$dir" ]; then
-        jre_entries=("$(ls "$dir")")
-        jre=1
-        update_alternatives "$dir" "jre_entries[@]"
-    fi
-
-    dir="$java_home/bin"
-    if [ -d "$dir" ]; then
-        jdk_entries=("$(ls "$dir")")
-
-        # If jre dir is present then remove duplicate binaries from bin dir
-        if [ $jre == 1 ]; then
-            unique_jdk_entries=()
-            for jdk_entry in "${jdk_entries[@]}"; do
-                found=0
-                for jre_entry in "${jre_entries[@]}"; do
-                    if [ "$jdk_entry" == "$jre_entry" ]; then
-                        found=1
-                        break
-                    fi
-                done
-                if [ $found == 0 ]; then
-                    # shellcheck disable=SC2034
-                    unique_jdk_entries[i]=$jdk_entry
-                    i=$((i + 1))
-                fi
-            done
-            update_alternatives "$dir" "unique_jdk_entries[@]"
-        else
-            update_alternatives "$dir" "jdk_entries[@]"
-        fi
-    fi
-    export JAVA_HOME="$java_home"
-}
-
-update_alternatives() {
-    dir="$1"
-    priority=1
-
-    binaries=("${!2}")
-    for binary in "${binaries[@]}"; do
-        path="$dir/$binary"
-        if [[ "$binary" == "classic" ]] || [[ "$binary" == "j9vm" ]]; then
-            continue
-        fi
-        sudo update-alternatives --quiet --install "/usr/bin/$binary" "$binary" "$path" "$priority"
-        sudo update-alternatives --quiet --set "$binary" "$path"
-    done
-}
-
-print_home_of_openjdk6() {
-    echo "$OPENJDK6_JAVA_HOME"
-}
-
-print_home_of_openjdk7() {
-    echo "$OPENJDK7_JAVA_HOME"
-}
-
-print_home_of_openjdk8() {
-    echo "$OPENJDK8_JAVA_HOME"
-}
-
-print_home_of_oraclejdk7() {
-    echo "$ORACLEJDK7_JAVA_HOME"
-}
-
-print_home_of_oraclejdk8() {
-    echo "$ORACLEJDK8_JAVA_HOME"
-}
-
-print_home_of_oraclejdk9() {
-    echo "$ORACLEJDK9_JAVA_HOME"
-}
-
-print_home_of_oraclejdk10() {
-    echo "$ORACLEJDK10_JAVA_HOME"
-}
-
-print_home_of_ibmjava8() {
-    echo "$IBMJAVA8_JAVA_HOME"
-}
-
-print_home_of_openjdk8_openj9() {
-    echo "$OPENJDK8_OPENJ9_JAVA_HOME"
-}
-
-warn_sunjdk6_eol() {
-    echo "Sun/Oracle JDK 6 is EOL since November 2012, and is no longer supported. Please consider upgrading..." >&2
+print_home_of_openjdk() {
+    JAVA_HOME="$1"
+	echo "$JAVA_HOME"
 }
 
 warn_jdk_not_known() {
@@ -240,39 +81,20 @@ switch_jdk() {
             warn_gcj_user
             false
             ;;
-        openjdk6 | openjdk1.6 | openjdk1.6.0 | jdk6 | 1.6.0 | 1.6 | 6.0)
-            switch_to_openjdk6
-            ;;
-        openjdk7 | jdk7 | 1.7.0 | 1.7 | 7.0)
-            switch_to_openjdk7
-            ;;
         openjdk8 | jdk8 | 1.8.0 | 1.8 | 8.0)
-            switch_to_openjdk8
+            switch_to_specified_openjdk "$OPENJDK8_VERSION" "$OPENJDK8_JAVA_HOME"
             ;;
-        oraclejdk6 | oraclejdk1.6 | oraclejdk1.6.0 | oracle6 | oracle1.6.0 | oracle6.0 | sunjdk6 | sun6)
-            warn_sunjdk6_eol
-            false
+		openjdk11 | jdk11 | 1.11.0 | 1.11 | 11.0)
+            switch_to_specified_openjdk "$OPENJDK11_VERSION" "$OPENJDK11_JAVA_HOME"
             ;;
-        oraclejdk7 | oraclejdk1.7 | oraclejdk1.7.0 | oracle7 | oracle1.7.0 | oracle7.0 | oracle | sunjdk7 | sun7 | sun)
-            switch_to_oraclejdk7
+		openjdk12 | jdk12 | 1.12.0 | 1.12 | 12.0)
+            switch_to_specified_openjdk "$OPENJDK12_VERSION" "$OPENJDK12_JAVA_HOME"
             ;;
-        oraclejdk8 | oraclejdk1.8 | oraclejdk1.8.0 | oracle8 | oracle1.8.0 | oracle8.0)
-            switch_to_oraclejdk8
-            ;;
-        oraclejdk9 | oraclejdk1.9 | oraclejdk1.9.0 | oracle9 | oracle1.9.0 | oracle9.0)
-            switch_to_oraclejdk9
-            ;;
-        oraclejdk10 | oraclejdk1.10 | oraclejdk1.10.0 | oracle10 | oracle1.10.0 | oracle10.0)
-            switch_to_oraclejdk10
-            ;;
-        ibmjava8 | ibmjava1.8 | ibmjava1.8.0 | ibmjdk8 | ibmjdk1.8 | ibmjdk1.8.0 | ibm8 | ibm1.8.0 | ibm8.0)
-            switch_to_ibmjava8
-            ;;
-        openjdk8j9 | openjdk8openj9 | openjdk8withopenj9)
-            switch_to_openjdk8_openj9
+		openjdk13 | jdk13 | 1.13.0 | 1.13 | 13.0)
+            switch_to_specified_openjdk "$OPENJDK13_VERSION" "$OPENJDK13_JAVA_HOME"
             ;;
         default)
-            "switch_to_${JDK_SWITCHER_DEFAULT}"
+            switch_to_specified_openjdk "$OPENJDK8_VERSION" "$OPENJDK8_JAVA_HOME"
             ;;
         *)
             warn_jdk_not_known "$1"
@@ -289,38 +111,20 @@ print_java_home() {
         *gcj*)
             warn_gcj_user
             ;;
-        openjdk6 | openjdk1.6 | openjdk1.6.0 | jdk6 | 1.6.0 | 1.6 | 6.0)
-            print_home_of_openjdk6
+		openjdk8 | jdk8 | 1.8.0 | 1.8 | 8.0)
+            print_home_of_openjdk "$OPENJDK8_JAVA_HOME"
             ;;
-        openjdk7 | jdk7 | 1.7.0 | 1.7 | 7.0)
-            print_home_of_openjdk7
+		openjdk11 | jdk11 | 1.11.0 | 1.11 | 11.0)
+            print_home_of_openjdk "$OPENJDK11_JAVA_HOME"
             ;;
-        openjdk8 | jdk8 | 1.8.0 | 1.8 | 8.0)
-            print_home_of_openjdk8
+		openjdk12 | jdk12 | 1.12.0 | 1.12 | 12.0)
+            print_home_of_openjdk "$OPENJDK12_JAVA_HOME"
             ;;
-        oraclejdk6 | oraclejdk1.6 | oraclejdk1.6.0 | oracle6 | oracle1.6.0 | oracle6.0 | sunjdk6 | sun6)
-            warn_sunjdk6_eol
-            ;;
-        oraclejdk7 | oraclejdk1.7 | oraclejdk1.7.0 | oracle7 | oracle1.7.0 | oracle7.0 | oracle | sunjdk7 | sun7 | sun)
-            print_home_of_oraclejdk7
-            ;;
-        oraclejdk8 | oraclejdk1.8 | oraclejdk1.8.0 | oracle8 | oracle1.8.0 | oracle8.0)
-            print_home_of_oraclejdk8
-            ;;
-        oraclejdk9 | oraclejdk1.9 | oraclejdk1.9.0 | oracle9 | oracle1.9.0 | oracle9.0)
-            print_home_of_oraclejdk9
-            ;;
-        oraclejdk10 | oraclejdk1.10 | oraclejdk1.10.0 | oracle10 | oracle1.10.0 | oracle10.0)
-            print_home_of_oraclejdk10
-            ;;
-        ibmjava8 | ibmjava1.8 | ibmjava1.8.0 | ibmjdk8 | ibmjdk1.8 | ibmjdk1.8.0 | ibm8 | ibm1.8.0 | ibm8.0)
-            print_home_of_ibmjava8
-            ;;
-        openjdk8j9 | openjdk8openj9 | openjdk8withopenj9)
-            print_home_of_openjdk8_openj9
+		openjdk13 | jdk13 | 1.13.0 | 1.13 | 13.0)
+            print_home_of_openjdk "$OPENJDK13_JAVA_HOME"
             ;;
         default)
-            "print_home_of_${JDK_SWITCHER_DEFAULT}"
+            print_home_of_openjdk "$OPENJDK8_JAVA_HOME"
             ;;
         *)
             warn_jdk_not_known "$JDK"
